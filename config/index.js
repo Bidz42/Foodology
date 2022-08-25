fc4b3e184328220df898ef4070f1ddf204121a60
Bidz42 <abid97r@gmail.com>
2022-08-25 11:59:45 +0100

    mongo issue

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ module.exports = (app) => {
         maxAge: 1000 * 60 * 60 * 24, // 24h
       },
       store: new MongoStore({
-        mongoUrl: MONGODB_URI,
+        mongoUrl: process.env.MONGODB_URI,
         ttl: 60 * 60 * 24       
       })
     })
